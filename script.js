539e9b08e5853a91bb3ba2d1d77c9867caef9828
Kenenn Lara <kenennlara@gmail.com>
2026-03-24 16:09:16 -0600

    Add Hover effect
    If applied, this commit will add the ability for the grid to change color when hovering each square.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Create the divs using JavaScript.
-const container = document.querySelector(".container");
-
+let container = document.querySelector(".container");
 let total = 256;
 
 function createContainer() {
@@ -8,7 +7,26 @@ function createContainer() {
     const div = document.createElement("div");
     container.appendChild(div);
     div.classList.add("column");
+    changeColor(div);
   }
 }
 
+//Set up a “hover” effect so that the grid divs change color
+// when your mouse passes over them
+
+function changeColor(div) {
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "red";
+  });
+}
+
+/*
+function reverseColor(div) {
+  div.addEventListener("mouseout", () => {
+    div.style.backgroundColor = "pink";
+  });
+}
+  */
+
+//Create container
 createContainer();
