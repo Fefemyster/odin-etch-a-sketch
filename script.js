5f3d21c37b85ef4a0421773831d20e4c15c6e00a
Kenenn Lara <kenennlara@gmail.com>
2026-03-26 17:48:09 -0600

    Add grid creation limits
    Limit user input to a range of sizes between 16x16 to 100x100 for saving computer resources.

--- script.js
+++ script.js
@@ -25,24 +25,28 @@ function changeColor(div) {
   });
 }
 
-/*
-function reverseColor(div) {
-  div.addEventListener("mouseout", () => {
-    div.style.backgroundColor = "pink";
-  });
-}
-*/
-
 //Leave a 16x16 grid as default -- Done
 //The button should only activate when clicking it -- Done
 //The button should be able to generate a new grid -- Done
 // Remove the old grid and add the new grid -- Done
-// When creating a new sketch, the grid elements should take up all the space
+// When creating a new sketch, the grid elements should take up all the space - Done
+
+//Set the limit for the user input to a maximum of 100. -- Done
+//Set a minimum of 16x16; -- Done
+// Value input by user should be multiply by itself --- Done
 
 const button = document.querySelector(".btn");
 //Creating new container
 button.addEventListener("click", () => {
   let total = prompt("Enter the size of the grid");
-  container.replaceChildren();
-  createGrid(total);
+  if (total <= 16) {
+    alert("Grid should be bigger than the default 16x16 size");
+    return;
+  } else if (total > 100) {
+    alert("Grid should not exceed 100*100 size");
+    return;
+  } else {
+    container.replaceChildren();
+    createGrid(total * total);
+  }
 });
