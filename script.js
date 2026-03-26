e15029c238fe455c5f80c4289377f4ec2d40380f
Kenenn Lara <kenennlara@gmail.com>
2026-03-25 19:56:44 -0600

    Button now creates a new grid
    Run button to ask user for new grid size and generate it.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //Create the divs using JavaScript.
-let container = document.querySelector(".container");
-let total = 256;
+const container = document.querySelector(".container");
+//Add a button on the top of the screen that will send the user a popup asking for the
+// number of squares per side for the new grid.
 
-function createContainer() {
+let total = 256; //16*16 default size
+
+createGrid(total);
+
+function createGrid(total) {
   for (i = 0; i < total; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
@@ -26,7 +31,18 @@ function reverseColor(div) {
     div.style.backgroundColor = "pink";
   });
 }
-  */
+*/
 
-//Create container
-createContainer();
+//Leave a 16x16 grid as default -- Done
+//The button should only activate when clicking it -- Done
+//The button should be able to generate a new grid -- Done
+// Remove the old grid and add the new grid -- Done
+// When creating a new sketch, the grid elements should take up all the space
+
+const button = document.querySelector(".btn");
+//Creating new container
+button.addEventListener("click", () => {
+  let total = prompt("Enter the size of the grid");
+  container.replaceChildren();
+  createGrid(total);
+});
